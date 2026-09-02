d502dab44fc9e4587e7d87090c381b21139f1812
trinityhaisch <138032525+trinityhaisch@users.noreply.github.com>
2026-09-02 19:35:52 -0400

    final iteration for now

--- basic_solver_with_tests_template.m
+++ basic_solver_with_tests_template.m
@@ -10,21 +10,21 @@ function basic_solver_with_tests_template()
     xlabel('x'); ylabel('y'); title('Test Function 1');
 
     % %Newton's method example test
-     % x0_guess = 2;
-     % plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5);
+     x0_guess = 2;
+     plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5);
+
+     x_sol = newton_solver(@test_func01,x0_guess);
+     plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
      % 
-     % x_sol = newton_solver(@test_func01,x0_guess);
-     % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
-    
 
     %Secant method example test
-    x0_guess = -5;
-    x1_guess = 2;
-    plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5);
-    plot(x1_guess,test_func01(x1_guess),'ko','markerfacecolor','k','markersize',5);
-
-    x_sol = secant_solver(@test_func01,x0_guess,x1_guess);
-    plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
+    % x0_guess = -5;
+    % x1_guess = 2;
+    % plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5);
+    % plot(x1_guess,test_func01(x1_guess),'ko','markerfacecolor','k','markersize',5);
+    % 
+    % x_sol = secant_solver(@test_func01,x0_guess,x1_guess);
+    % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
 
     
     %Bisection method example test
@@ -51,17 +51,27 @@ end
 
 function x = bisection_solver(fun,x_left,x_right)
     x_mid=(x_left+x_right)/2;
-    while abs(fun(x_mid))>0.0005
-        
-        if fun(x_left)>0 && fun(x_mid)<0 || fun(x_left)<0 && fun(x_mid)>0
-            x_right=x_mid;
+    if (fun(x_left)<0 && fun(x_right)>0) || (fun(x_left)>0 && fun(x_right)<0)
 
-        else 
-            x_left=x_mid;
-        end
-        x=x_mid;
-        x_mid=(x_left+x_right)/2;
-    end 
+        while abs(fun(x_mid))>(1*10^-14)
+            
+            if fun(x_left)>0 && fun(x_mid)<0 || fun(x_left)<0 && fun(x_mid)>0
+                x_right=x_mid;
+    
+            else 
+                x_left=x_mid;
+            end
+            x=x_mid;
+            x_mid=(x_left+x_right)/2;
+    
+            if abs(x-x_mid)<1*10^-14 && abs(fun(x_mid))<1*10^-14
+                return
+            end
+        end 
+    else
+        x=0;
+        return
+    end
    
     return;
 end
@@ -69,11 +79,18 @@ end
 %Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
 function xn = newton_solver(fun,x0)
     xn=x0;
-    while abs(fun(xn))>0.0005
+    while abs(fun(xn))>0.000000005
 
         [fn, dfn]= fun(xn);
+        if abs(dfn)<0.000000000000000000000005
+            return
+        end
         xn1= xn- fn/dfn;
         xn=xn1;
+
+        if abs(xn1-xn)<1*10^-14 && abs(fun(xn))<1*10^-14
+            return
+        end
     end 
    
     return;
@@ -83,12 +100,20 @@ function xn = secant_solver(fun,x0, x1)
     xn2=x0;
     xn1=x1;
     xn = xn1;
-    while abs(fun(xn))>0.0005
+    while abs(fun(xn))>1*10^-14
+
+        if abs(fun(xn1)-fun(xn2))<0.000000000000000000000005
+            return
+        end
         xn = xn1-fun(xn1)*((xn1-xn2)/(fun(xn1)-fun(xn2)));
         xn2=xn1;
         xn1=xn;
+
+        if abs(xn1-xn)<1*10^-14 && abs(fun(xn))<1*10^-14
+            return
+        end
     end 
-    xn
+ 
     return;
 end
 
