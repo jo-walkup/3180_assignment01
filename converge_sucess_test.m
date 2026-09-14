1a6a02261a2a5bfeaa608f2e5dc359f47d533d78
jo-walkup <jwalkup@olin.edu>
2026-09-13 21:24:55 -0400

    newton's method convergence plot done
    yayyyy

--- converge_sucess_test.m
+++ converge_sucess_test.m
@@ -4,65 +4,80 @@ function converge_sucess_test()
     [yvals,~] = test_func03(xvals);
 
 
-    hold on
+    % hold on
  
-    axis([-15,40,-50,80]);
-    plot(xvals,yvals,'r','linewidth',2);
-    plot(xvals,0*xvals,'k--','linewidth',1);
-    xlabel('x'); ylabel('y'); title('Test Function 3');
+    % axis([-15,40,-50,80]);
+    % plot(xvals,yvals,'r','linewidth',2);
+    % plot(xvals,0*xvals,'k--','linewidth',1);
+    % xlabel('x'); ylabel('y'); title('Test Function 3');
   
-    x_left=linspace(1,23,1000);
-    x_right=linspace(27,50,1000);
-
-    total_x_roots=[];
-
-    [x, y]=meshgrid(x_left, x_right);
-
-    % en_list=[];
-    % en1_list=[];
-    % index_list = []; %line 36
-    % figure()
     
-    for i=1:length(x_left)*length(x_right)
+    s=[];
+    f=[];
 
-    [x_root, ~] = bisection_solver(@test_func03,x_left(i),x_right(i));
+    newton_guess = linspace(1,50,1000);
+
+    for i = 1:length(newton_guess)
+        [x_root, ~] = fzero_solver(@test_func03,newton_guess(i));
+        
+        if abs(x_root-26.1618)<0.0003
+            s=[s, newton_guess(i)];
+        else
+            f=[f,newton_guess(i)];
+
+        end
+    end   
+
+    [fs,~] = test_func03(s);
+    [ff,~] = test_func03(f);
     
-    total_x_roots=[total_x_roots, x_root]
+    plot(s,fs, "b."); hold on
+    plot(f,ff,"r.")
+    plot(26.1618, 0, "k.", "MarkerSize", 20)
+    plot(0:50, (0:50)*0, "k--")
+    xlabel("Input x")
+    ylabel("Output f(x)")
+    legend("Converging guesses", "Diverging guesses", "Root", "Location", "northwest")
+    title("Newton's Method Guess Convergence Plot")
 
-    % en=abs(x_list-x_sol);
-    % en1=abs(x_listn-x_sol);
-    % en_list=[en_list,en];
-    % en1_list=[en1_list,en1];
+    % fail = [];
+    % faily = [];
+    % success = [];
+    % successy = [];
+    % length(total_x_roots)
     % 
-    % index_list = [index_list,1:length(en)];
     % 
-    % end
-    % 
-    % x_regression = []; % e_n
-    % y_regression = []; % e_{n+1}
-    % filter_list = [1e-15, 1e-2, 1e-14, 1e-2, 2];
-    % % iterate through the collected data
-    % for n=1:length(index_list)
-    % 
-    %     if en_list(n)>filter_list(1) && en_list(n)<filter_list(2) && ...
-    %     en1_list(n)>filter_list(3) && en1_list(n)<filter_list(4) && ...
-    %     index_list(n)>filter_list(5)
-    %     %then add it to the set of points for regression
-    %     x_regression(end+1) = en_list(n);
-    %     y_regression(end+1) = en1_list(n);
+    % for i = 1:length(total_x_roots)
+    %     root = total_x_roots(i)
+    %     if root == 0
+    %         disp("wtf")
+    %         fail = [fail, root];
+    %         faily = [faily, i];
+    %     else
+    %         disp("help")
+    %         root
+    %         success(end+1) = root;
+    %         successy = [successy, i];
     %     end
-    end
+    % end
+    % success
+    % successy
+    % fail;
 
-    hold off; 
-
-
-    % loglog(en_list,en1_list,'r.', 'MarkerSize',5 ); hold on
-    % loglog(x_regression,y_regression,'b.', 'MarkerSize',5);
-    
-    scatter(x,y, "r")
-    title("Bisection Method Error")
-    xlabel('x')
-    ylabel('x')
+    % axis([-15,40,-50,80]);
+    % % plot(fail,faily,'r','linewidth',2); hold on
+    % plot(success,successy,'b*','linewidth',2); hold on
+    % plot(xvals,yvals,'r','linewidth',2);
+    % plot(xvals,0*xvals,'k--','linewidth',1);
+    % 
+    % xlabel('x'); ylabel('y'); title('Test Function 3');
+    % % loglog(en_list,en1_list,'r.', 'MarkerSize',5 ); hold on
+    % % loglog(x_regression,y_regression,'b.', 'MarkerSize',5);
+    % figure();
+    % scatter(x,y, "r")
+    % title("Bisection Method Error")
+    % xlabel('x')
+    % ylabel('x')
 
 end
 
